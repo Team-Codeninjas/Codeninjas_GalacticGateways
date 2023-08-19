561e63dbd7485df059927e4c3de0b4ca7b0f4430
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-08-19 23:49:15 +0530

    changed db name

--- backend/configs/connect-db.js
+++ backend/configs/connect-db.js
@@ -1,6 +1,6 @@
 const {mongoose} = require("mongoose");
 
-mongoose.connect('mongodb://0.0.0.0:27017/galactic-gateways', {
+mongoose.connect('mongodb://0.0.0.0:27017/GalacticGateways', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
