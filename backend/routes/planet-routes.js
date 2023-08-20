b113719c293c090181da45eb81bb8a883299808c
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-08-21 01:20:43 +0530

    minor fix

--- backend/routes/planet-routes.js
+++ backend/routes/planet-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const planetFunc = require('../controllers/planet-controller.js');
 
-router.post('/create', planetFunc.createPlanet);
+router.post('/', planetFunc.createPlanet);
 
 
 module.exports = router;
