90a4aa506c98e1458b7d5efa05048863d61b79f8
vinod liyanage <vinodsliyanage@gmail.com>
2023-08-20 17:04:18 +0530

    add App.js

---
+++ frontend/App.js
@@ -0,0 +1,23 @@
+import AppNavigator from "./navigation/AppNavigator";
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Button, StyleSheet, Text, View, StyleSheet } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {},
+});
