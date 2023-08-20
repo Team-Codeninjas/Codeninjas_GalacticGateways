53604f4f371a477e79c3d45fa66fec08da654dc4
vinod liyanage <vinodsliyanage@gmail.com>
2023-08-20 11:45:03 +0530

    remove ThemeProvider.

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,25 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { SplashScreen, Stack } from "expo-router";
-import { useColorScheme } from "react-native";
 
 export { ErrorBoundary } from "expo-router";
 
 // SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  // Load fonts, then hide the splash screen.\
+  // Load fonts, then hide the splash screen.
   // SplashScreen.hideAsync()
   return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
