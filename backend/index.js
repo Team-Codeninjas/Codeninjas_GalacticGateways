928eb6696a5539a5dbe50963be3ccbf5e2e03f43
ChethiyaNuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2023-08-20 23:41:46 +0530

    added routes for travel class, spaceship, schedule

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ app.use("/users", require("./routes/user-routes.js"));
 app.use("/monetary", require("./routes/monetary-routes.js"));
 app.use("/spaceports", require("./routes/spaceport-routes.js"));
 app.use("/planets", require("./routes/planet-routes.js"));
+app.use("/schedules", require("./routes/schedule-routes.js"));
+app.use("/travel-classes", require("./routes/travelclass-routes.js"));
+app.use("/spaceships", require("./routes/spaceship-routes.js"));
 
 
 const PORT = 5000;
