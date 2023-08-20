f3675936b713fe7e2b91c5784b3ac79183424a63
yukthi hettiarachchi <101061736+yukthi2000@users.noreply.github.com>
2023-08-20 16:39:51 +0530

    navigation

--- frontend/App.js
+++ frontend/App.js
@@ -4,12 +4,9 @@ import AppNavigator from './navigation/AppNavigator';
 import React from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ChargesPage from "./pages/Charges/Chargespage";
-import Payment from "./pages/Payment";
+
 import { Button, StyleSheet, Text, View,StyleSheet } from "react-native";
-import Details from "./pages/Details";
-import CustomImageCarousal from "./components/CustomImageCarousalSquare";
-import TicketPage from './screens/ticketDownload/TicketPage'
+
 
 const Stack=createNativeStackNavigator();
 
@@ -23,19 +20,6 @@ const App = () => {
 };
 
 export default App;
-
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ChargesPage} options={{ headerShown: false ,animation: 'slide_from_bottom'}}  />
-        <Stack.Screen name="Payment" component={Payment}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
-        <Stack.Screen name="CustomImageCarousal" component={CustomImageCarousal}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
-        <Stack.Screen name="Ticket" component={TicketPage}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
-        <Stack.Screen name="Details" component={Details}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-
-}
 
 
 const styles = StyleSheet.create({
--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import TicketBookingHome from '../screens/TicketBooking/TicketBookingHome';
 import TicketBookingDetails from '../screens/TicketBooking/TicketBookingDetails';
+import Payment from '../pages/Payment';
+import CustomImageCarousal from '../components/CustomImageCarousalSquare';
+import TicketPage from '../screens/ticketDownload/TicketPage';
+import Details from '../pages/Details';
 
 const Stack = createStackNavigator();
 
@@ -19,7 +23,11 @@ const AppNavigator = () => {
       <Stack.Screen name="TicketBookingDetails"
         component={TicketBookingDetails}
         options={{ headerTitle: '', headerShown: false }} />
-      
+             <Stack.Screen name="Home" component={ChargesPage} options={{ headerShown: false ,animation: 'slide_from_bottom'}}  />
+        <Stack.Screen name="Payment" component={Payment}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
+        <Stack.Screen name="CustomImageCarousal" component={CustomImageCarousal}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
+        <Stack.Screen name="Ticket" component={TicketPage}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
+        <Stack.Screen name="Details" component={Details}  options={{ headerShown: false,animation: 'slide_from_right' }}/>
     </Stack.Navigator>
   );
 };
