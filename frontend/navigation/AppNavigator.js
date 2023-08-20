5247acbe36c091dd37b8f87cecc2a3724b2b67cd
yukthi hettiarachchi <101061736+yukthi2000@users.noreply.github.com>
2023-08-20 17:46:45 +0530

    charges add

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -4,15 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import TicketBookingHome from '../screens/TicketBooking/TicketBookingHome';
 import TicketBookingDetails from '../screens/TicketBooking/TicketBookingDetails';
 import Form from '../pages/Form';
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import TicketBookingHome from "../screens/TicketBooking/TicketBookingHome";
-import TicketBookingDetails from "../screens/TicketBooking/TicketBookingDetails";
+// import React from "react";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import TicketBookingHome from "../screens/TicketBooking/TicketBookingHome";
+// import TicketBookingDetails from "../screens/TicketBooking/TicketBookingDetails";
 import Payment from "../pages/Payment";
 import CustomImageCarousal from "../components/CustomImageCarousalSquare";
 import TicketPage from "../screens/ticketDownload/TicketPage";
 import Details from "../pages/Details";
 import Home from "../pages/Home";
+import ChargesPage from '../pages/Charges/Chargespage';
 
 const Stack = createStackNavigator();
 
@@ -42,6 +43,11 @@ const AppNavigator = () => {
         name="Home"
         component={Home}
         options={{ headerShown: false, animation: "slide_from_bottom" }}
+      />
+      <Stack.Screen
+        name="ChargesPage"
+        component={ChargesPage}
+        options={{ headerShown: false, animation: "slide_from_right" }}
       />
       <Stack.Screen
         name="Payment"
